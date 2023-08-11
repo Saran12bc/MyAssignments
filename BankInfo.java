025790fad002e889fc736c7081e2efe59daa4c81
Saranya Sivasamy <141121415+Saran12bc@users.noreply.github.com>
2023-08-11 13:44:23 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,17 @@
+package week3day1;
+
+public class BankInfo
+{
+
+	public void saving() {
+		System.out.println("savings");
+	}
+	public void fixed() {
+		System.out.println("fixed");
+	}
+	public void deposit() {
+		System.out.println("deposit");
+
+	}
+
+}
